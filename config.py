a5390fb4f58c8f704bd288b4e1415ac349fb1b6b
dodeoy <Mohmmedatef933@gmail.com>
2024-10-27 01:33:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -50,7 +50,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ChampuXD/ChampuMusic",
+    "https://github.com/dodeoy/dd",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
